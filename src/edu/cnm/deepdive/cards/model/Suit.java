c0a4cbac11082f44432ee8bebd4e232c718c1289
Quentin-D-NM <qdye@cnm.edu>
2019-09-30 15:52:21 -0600

    Enhanced cards model. Added Color enum to Suit; added Pile class (should Deck be a subclass of pile?); implemented silly card trick.

--- src/edu/cnm/deepdive/cards/model/Suit.java
+++ src/edu/cnm/deepdive/cards/model/Suit.java
@@ -12,4 +12,12 @@ public enum Suit {
   public String getSymbol() {
     return SYMBOLS[ordinal()];
   }
+
+  public Color getColor() {
+    return (ordinal() % 3 == 0) ? Color.BLACK : Color.RED;
+  }
+
+  public enum Color {
+    BLACK, RED;
+  }
 }
